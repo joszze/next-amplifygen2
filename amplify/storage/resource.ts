75a6e810eb77620dd07c4216f8ce0dcae2da027d
joszze <jossecarlosg@gmail.com>
2024-12-16 11:37:45 -0500

    fix bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,4 +3,10 @@ import { defineStorage } from "@aws-amplify/backend";
 export const firstBucket = defineStorage({
   name: "firstBucket",
   isDefault: true, // identify your default storage bucket (required)
+  access: (allow) => ({
+    "public/*": [
+      allow.guest.to(["read"]),
+      allow.authenticated.to(["read", "write", "delete"]),
+    ],
+  }),
 });
